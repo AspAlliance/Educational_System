53dbe494a58055d4f2a3edc883b6a6c7ef578f5f
Amr <amrshawky936@gmail>
2024-11-25 00:16:32 +0200

    Revert "test"
    This reverts commit 0c163c5437b8b2c0a85772fb37237a0306d1ea2c.

--- EducationalSystem.DAL/Models/User_Instructor.cs
+++ EducationalSystem.DAL/Models/User_Instructor.cs
@@ -4,7 +4,7 @@
     {
         public string UserId { get; set; } // ForeignKey with ApplicationUser
         public int InstructorId { get; set; } // ForeignKey with Instructors
-        //test
+
         public Instructors Instructors { get; set; }
         public ApplicationUser User { get; set; }
     }

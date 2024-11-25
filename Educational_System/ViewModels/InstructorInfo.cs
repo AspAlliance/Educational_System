cea3a0dbbfc61d509cd95cc407c59b4ac14f0ab6
Amr <amrshawky936@gmail>
2024-11-25 16:45:51 +0200

    Add Action GitAll That Get specific data of instructors of type Instructor info in view model

---
+++ Educational_System/ViewModels/InstructorInfo.cs
@@ -0,0 +1,12 @@
+﻿using EducationalSystem.DAL.Models;
+
+namespace Educational_System.ViewModels
+{
+    public class InstructorInfo
+    {
+        public string InstructorName { get; set; }
+        public string SpecializationsName { get; set; }
+        public string BIO { get; set; }
+        public List<Courses> Courses { get; set; } = new List<Courses>();
+    }
+}

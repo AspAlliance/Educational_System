395d2db672493a5b69ef32a6bcfc8f855f6a9ca6
mu-hammad-ibrahim <mohamed_ibrahim1999@outlook.com>
2024-11-26 23:39:31 +0200

    add password options for Identity services make repositories scope more clean

--- Educational_System/Program.cs
+++ Educational_System/Program.cs
@@ -4,6 +4,7 @@ using EducationalSystem.DAL.Models;
 using EducationalSystem.DAL.Models.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace EducationalSystem
 {
@@ -22,14 +23,42 @@ namespace EducationalSystem
                     b => b.MigrationsAssembly("EducationalSystem.DAL")));  // Specify the migrations assembly here
 
             // Register Identity services
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                //options.SignIn.RequireConfirmedAccount = true;
+                // Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 0;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+            })
                 .AddEntityFrameworkStores<Education_System>()
                 .AddDefaultTokenProviders();  // Registers the UserManager and other Identity-related services
 
             // Add scoped repositories for dependency injection
-            builder.Services.AddScoped<IGenericRepository<Instructors>, GenericReposiory<Instructors>>();
-            builder.Services.AddScoped<IGenericRepository<Courses>, GenericReposiory<Courses>>();
-            builder.Services.AddScoped<IGenericRepository<Course_Instructors>, GenericReposiory<Course_Instructors>>();
+
+            //scoped repositories Clean
+            var entityTypes = Assembly.GetAssembly(typeof(BaseEntity))
+                .GetTypes()
+                .Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(BaseEntity)))
+                .ToList();
+
+            foreach (var entityType in entityTypes)
+            {
+                var interfaceType = typeof(IGenericRepository<>).MakeGenericType(entityType);
+                var implementationType = typeof(GenericReposiory<>).MakeGenericType(entityType);
+                builder.Services.AddScoped(interfaceType, implementationType);
+            }
+
+            //builder.Services.AddScoped<IGenericRepository<Instructors>, GenericReposiory<Instructors>>();
+            //builder.Services.AddScoped<IGenericRepository<Courses>, GenericReposiory<Courses>>();
+            //builder.Services.AddScoped<IGenericRepository<Course_Instructors>, GenericReposiory<Course_Instructors>>();
 
             // Add Swagger for API documentation
             builder.Services.AddEndpointsApiExplorer();

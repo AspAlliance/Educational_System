daf0d5b05622af90cab62425b2f86605d522910c
mu-hammad-ibrahim <mohamed_ibrahim1999@outlook.com>
2024-12-03 01:00:51 +0200

    Created Specification interface and implemented InstructorSpecification for filtering instructor data.

---
+++ EducationalSystem.BLL/Specification/ISpecification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalSystem.BLL.Specification
+{
+    public interface ISpecification<T>
+    {
+        IQueryable<T> Apply(IQueryable<T> query);
+    }
+}

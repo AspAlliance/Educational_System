3e172983b0d6c6a808a06be34c989b9610b3515c
AMR-LORD <amrshawky936@gmail.com>
2024-12-10 16:04:49 +0200

    solve issue

--- Educational_System/Helpers/MappingProfiles.cs
+++ Educational_System/Helpers/MappingProfiles.cs
@@ -13,12 +13,17 @@ namespace Educational_System.Helpers
                 .ReverseMap();
             CreateMap<RegisterBS, ApplicationUser>()
                 .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.Password));
+            CreateMap<RegisterInstructorDto, ApplicationUser>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.Password))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber));
 
             CreateMap<Instructors, InstructorsDto>()
                 .ForMember(dest => dest.InstructorName, opt => opt.MapFrom(src => src.applicationUser.Name))
-                .ForMember(dest => dest.SpecializationsName, opt => opt.MapFrom(src => src.Specializations.SpecializationName)) 
+                .ForMember(dest => dest.SpecializationsName, opt => opt.MapFrom(src => src.Specializations.SpecializationName))
                 .ForMember(dest => dest.Courses, opt => opt.MapFrom(src => src.Course_Instructors.Select(ci => ci.Courses).ToList()))
-              //.ForMember(dest => dest.CourseTitles, opt => opt.MapFrom(src => src.Course_Instructors.Select(ci => ci.Courses.CourseTitle).ToList()))
+                //.ForMember(dest => dest.CourseTitles, opt => opt.MapFrom(src => src.Course_Instructors.Select(ci => ci.Courses.CourseTitle).ToList()))
                 .ReverseMap();
 
 

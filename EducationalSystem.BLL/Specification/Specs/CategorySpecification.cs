381a4c9c8f3ce81601196a453c30faa2c14d1b7d
mu-hammad-ibrahim <mohamed_ibrahim1999@outlook.com>
2024-12-20 20:01:33 +0200

    implement CategorySpecification

---
+++ EducationalSystem.BLL/Specification/Specs/CategorySpecification.cs
@@ -0,0 +1,18 @@
+﻿using EducationalSystem.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalSystem.BLL.Specification.Specs
+{
+    public class CategorySpecification : ISpecification<Categories>
+    {
+        public IQueryable<Categories> Apply(IQueryable<Categories> query)
+        {
+            return query.Include(c => c.Courses);
+        }
+    }
+}
